72d268e2cf18ffde92f7fdf27acecd8d4fef8f88
Nazaret <nazaretgutirrezmartn@gmail.com>
2025-03-21 11:15:27 +0100

    separación componente botón abr/cer

---
+++ src/components/ButtonOpenPokedex.jsx
@@ -0,0 +1,5 @@
+const ButtonOpenPokedex = ({ toggleOpen, isOpen }) => {
+  return <button className="button" onClick={toggleOpen}></button>;
+};
+
+export default ButtonOpenPokedex;
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,41 +1,34 @@
+import { useState } from "react";
+import ButtonOpenPokedex from "./ButtonOpenPokedex";
 import "../styles/pokedex.css";
 
-import { useState } from "react";
-
 const Pokedex = () => {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const toggleOpen = () => {
-		setIsOpen(!isOpen);
-	};
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <section className="container">
+      <div className="console">
+        {/* Pantalla superior */}
+        <div className={`top-screen ${isOpen ? "open" : "closed"}`}>
+          <div className="screen-inner">
+            <div className="screen-content"></div>
+          </div>
+        </div>
 
-	return (
-		<section className="container">
-			<div className="console">
-				{/* Pantalla superior */}
-				<div className={`top-screen ${isOpen ? "open" : "closed"}`}>
-					<div className="screen-inner">
-						<div className="screen-content">
-							<span className="screen-text">NINTENDO DS</span>
-						</div>
-					</div>
-				</div>
-
-				{/* Pantalla inferior */}
-				<div className="bottom-screen">
-					<div className="screen-inner">
-						<div className="screen-content">
-							<button className="button" onClick={toggleOpen}>
-								{isOpen ? "Cerrar" : "Abrir"}
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			{/* Botón para abrir/cerrar */}
-		</section>
-	);
+        {/* Pantalla inferior */}
+        <div className="bottom-screen">
+          <div className="bottom-screen-inner">
+            <div className="bottom-screen-content">
+              <ButtonOpenPokedex toggleOpen={toggleOpen} isOpen={isOpen} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Pokedex;
